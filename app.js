726405413d10a3b69e46909d4df53e4eea2c251b
Leon Smith Jr <creativeinspiration101@gmail.com>
2022-03-22 17:04:21 -0400

    Commit after project Completion

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+
+app.get("/", (req,res) => {res.render('home')});
+
+app.listen(3000, () => console.log("Press X to Start"));
